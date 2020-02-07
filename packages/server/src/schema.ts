257f062da0adcc584c04608281e306e182af7d53
gh0stonio <antoine.leveque.pro@gmail.com>
2020-02-07 16:02:23 +0100

    feat: implement apollo server with dummy data
    affects: @codingnews/server

---
+++ packages/server/src/schema.ts
@@ -0,0 +1,32 @@
+import { gql } from 'apollo-server'
+import { Resolvers, News } from './__generated__/types'
+
+const news: News[] = [
+  {
+    title: 'Top 6 Material Design Dashboards',
+    link: 'https://dev.to/anaflatlogic/top-6-material-design-dashboards-1mhl',
+  },
+  {
+    title: 'Dockerization of Node.JS Applications on Amazon Elastic Containers',
+    link: 'https://blog.soshace.com/dockerization-of-node-js-applications-on-amazon-elastic-containers/',
+  },
+]
+
+const typeDefs = gql`
+  type News {
+    title: String!
+    link: String!
+  }
+
+  type Query {
+    news: [News]
+  }
+`
+
+const resolvers: Resolvers = {
+  Query: {
+    news: () => news,
+  },
+}
+
+export default { typeDefs, resolvers }
